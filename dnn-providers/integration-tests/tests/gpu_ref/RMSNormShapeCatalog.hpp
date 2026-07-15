e24440256a5bd8813fdce132bd17b1f46987c871
Sai Kubair Kota <sai.kota@streamhpc.com>
2026-07-15 09:48:28 +0000

    Moved RMSNormTestCases to a separate file for consistency with other operations

--- dnn-providers/integration-tests/tests/gpu_ref/RMSNormShapeCatalog.hpp
+++ dnn-providers/integration-tests/tests/gpu_ref/RMSNormShapeCatalog.hpp
@@ -3,47 +3,12 @@
 
 #pragma once
 
-#include <gtest/gtest.h>
-#include <hipdnn_data_sdk/utilities/Tensor.hpp>
-#include <hipdnn_test_sdk/utilities/CpuFpReferenceValidation.hpp>
-
-#include <cassert>
-#include <cstdint>
-#include <ostream>
-#include <vector>
+#include "RMSNormTestCase.hpp"
 
 namespace gpu_rmsnorm_ref_test
 {
 
 using hipdnn_data_sdk::utilities::TensorLayout;
-
-struct RMSNormTestCase
-{
-    std::vector<int64_t> ioDims;
-    std::vector<int64_t> scaleDims;
-    TensorLayout layout;
-
-    friend std::ostream& operator<<(std::ostream& os, const RMSNormTestCase& tc)
-    {
-        os << "(io dims: ";
-        hipdnn_data_sdk::utilities::vecToStream(os, tc.ioDims);
-        os << " scale dims: ";
-        hipdnn_data_sdk::utilities::vecToStream(os, tc.scaleDims);
-        os << " layout:" << tc.layout.name;
-        os << ")";
-
-        return os;
-    }
-};
-
-template <typename T>
-void assertAllClose(hipdnn_data_sdk::utilities::TensorBase<T>& expected,
-                    hipdnn_data_sdk::utilities::TensorBase<T>& actual,
-                    float tolerance)
-{
-    auto validator = hipdnn_test_sdk::utilities::CpuFpReferenceValidation<T>(tolerance, 0.0f);
-    ASSERT_TRUE(validator.allClose(expected, actual));
-}
 
 inline std::vector<RMSNormTestCase> getRMSnormTestCases()
 {
